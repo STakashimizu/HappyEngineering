9283b7c474a07681660089212845d540144f4f6b
Satoru Takashimizu <kor19952006@gmail.com>
2015-10-04 18:04:41 +0900

    begin here

---
+++ happyEngineering/happyEngineering.m
@@ -0,0 +1,16 @@
+(* Mathematica Package *)
+
+(* Created by the Wolfram Workbench Oct 4, 2015 *)
+
+BeginPackage["happyEngineering`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
